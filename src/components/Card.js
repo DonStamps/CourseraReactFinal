81cf58b78ca084e28bcd88d945e5016a41ec4bfd
Donald Stamps <dstamps@dstamps.net>
2023-04-02 13:08:31 -0700

    Initial upload to GitHub

---
+++ src/components/Card.js
@@ -0,0 +1,36 @@
+import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+const Card = ({ title, description, imageSrc }) => {
+  // Implement the UI for the Card component according to the instructions.
+  // You should be able to implement the component with the elements imported above.
+  // Feel free to import other UI components from Chakra UI if you wish to.
+  return (
+    <VStack
+      spacing={4}
+      p={4}
+      textColor="black"
+      backgroundColor="white"
+      borderRadius="md"
+      boxShadow="md"
+    >
+      <Image src={imageSrc} alt={title} borderRadius="md" />
+      <VStack spacing={2} alignItems="flex-start">
+        <Heading size="md" fontWeight="700">
+          {title}
+        </Heading>
+        <Text fontWeight="400" color="blue.600">
+          {description}
+        </Text>
+      </VStack>
+      <HStack alignItems="left">
+        <Text fontWeight="400">See more</Text>
+        <FontAwesomeIcon icon={faArrowRight} size="1x" />
+      </HStack>
+    </VStack>
+  );
+};
+
+export default Card;
---
+++ src/components/LandingSection.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+
+const greeting = "Hi, I'm Don!";
+const bio1 = "Thank you for visiting my website.";
+const bio2 =
+  "A little bit about me. I am a frontend developer skilled in HTML, CSS, and React with 20+ years experience across all aspects of informaton technology from software development to system architecture, security, and site reliability.";
+
+// Implement the UI for the LandingSection component according to the instructions.
+// Use a combination of Avatar, Heading and VStack components.
+
+const avatarUrl = "https://i.pravatar.cc/150?img=69";
+
+const LandingSection = () => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+    <VStack spacing={4}>
+      <Avatar size="2xl" src={avatarUrl} />
+      <Heading size="lg">{greeting}</Heading>
+      <Heading size="md" fontWeight="normal" maxWidth={80}>
+        {bio1}
+      </Heading>
+      <Heading size="md" fontWeight="normal" maxWidth={80}>
+        {bio2}
+      </Heading>
+    </VStack>
+  </FullScreenSection>
+);
+
+export default LandingSection;
