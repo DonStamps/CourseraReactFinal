309c68f114247f8bf82b20da5b997c4345673fcf
Donald Stamps <dstamps@dstamps.net>
2023-04-08 17:24:45 -0700

    modified:   src/components/Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,6 +7,8 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
+  // You can also use the CSS-in-JS library styled-components if you wish to.
+
   return (
     <VStack
       spacing={4}
